01aa26969724799df0b1c81a5699a21691b70ae1
Sam Espinoza <sespino3@stumail.jccc.edu>
2023-02-26 16:19:14 -0600

    added few screens and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_pets_app/screens/splash_screen.dart';
 
+import 'screens/pet_favorites.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,11 +18,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const SplashScreen(),
+      // routes
+      routes: {
+        '/favorite_pets': (context) => const PetFavoritesPage(),
+      },
     );
   }
 }
