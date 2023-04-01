1ce67f0c80d1d11e6288a1afdfe17bab3996ce53
Travis Russell <trusse26@stumail.jccc.edu>
2023-04-01 15:02:43 -0500

    Finished favorites repo

--- lib/repositories/favorites_repository.dart
+++ lib/repositories/favorites_repository.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 import '../constants/db_constants.dart';
 import '../models/custom_error.dart';
+import '../models/favorite_model.dart';
 
 class FavoriteRepository {
   final FirebaseFirestore firebaseFirestore;
@@ -16,15 +17,25 @@ class FavoriteRepository {
   // before continuing with this file, complete the Favorite model in lib\models\favorite_model.dart - DONE
   // once Favorite model has been created replace the return type of getFavorites() with Future<List<Favorite>>
   // and return the correct data
-  Future<List<String>> getFavorites() async {
+  Future<List<Favorite>> getFavorites() async {
     try {
       // 1. get the current user id using FirebaseAuth.instance.currentUser!.uid assign it to a final variable named uid
+      final uid = firebaseAuth.currentUser!.uid;
       // 2. create a new final variable of type QuerySnapshot named favoriteList and assign it to await usersRef.doc(uid).collection('favorites').get();
+      final QuerySnapshot favoriteList =
+          await usersRef.doc(uid).collection('favorites').get();
       // 3. create an if block to check if favoriteList.docs.isNotEmpty
-      // 4. within the if block, create a new final variable called favoriteListData and assign it to favoriteList.docs.map((favoriteDoc) => Favorite.fromDoc(favoriteDoc)).toList();
-      // 5. return favoriteListData;
-      // 6. else return an empty list
-      return []; // DELETE THIS LINE
+      if (favoriteList.docs.isNotEmpty) {
+        // 4. within the if block, create a new final variable called favoriteListData and assign it to favoriteList.docs.map((favoriteDoc) => Favorite.fromDoc(favoriteDoc)).toList();
+        final favoriteListData = favoriteList.docs
+            .map((favoriteDoc) => Favorite.fromDoc(favoriteDoc))
+            .toList();
+        // 5. return favoriteListData;
+        return favoriteListData;
+      } else {
+        // 6. else return an empty list
+        return [];
+      }
     } on FirebaseException catch (e) {
       throw CustomError(
         code: e.code,
@@ -60,6 +71,44 @@ class FavoriteRepository {
   // TODO: create a new method named addFavorite() that takes a {required Favorite favorite} object as a parameter
   // when adding a favorite, inside the .set() method, you need to pass the favorite object as a map
   // calling the favorite toDoc method like, .set(favorite.toDoc(favorite));.
+  Future<void> addFavorite(Favorite favorite) async {
+    try {
+      final uid = firebaseAuth.currentUser!.uid;
+      await usersRef.doc(uid).set(favorite.toDoc(favorite));
+    } on FirebaseException catch (e) {
+      throw CustomError(
+        code: e.code,
+        message: e.message!,
+        plugin: e.plugin,
+      );
+    } catch (e) {
+      throw CustomError(
+        code: 'Exception',
+        message: e.toString(),
+        plugin: 'flutter_error/server_error.getFavorites',
+      );
+    }
+  }
 
   // TODO: create a new method named removeFavorite() that takes a String named favoriteId as a parameter
+  Future<void> removeFavorite(favoriteId) async {
+    try {
+      final uid = firebaseAuth.currentUser!.uid;
+      final QuerySnapshot favoriteList =
+          await usersRef.doc(uid).collection('favorites').get();
+      favoriteList.docChanges.remove(favoriteId);
+    } on FirebaseException catch (e) {
+      throw CustomError(
+        code: e.code,
+        message: e.message!,
+        plugin: e.plugin,
+      );
+    } catch (e) {
+      throw CustomError(
+        code: 'Exception',
+        message: e.toString(),
+        plugin: 'flutter_error/server_error.getFavorites',
+      );
+    }
+  }
 }
